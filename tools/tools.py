8a8d20bcfd9312e85385495b5aadf3e3fb5ff280
Hyoyin Gan <37298109+hgan0@users.noreply.github.com>
2023-08-18 17:36:06 -0700

    feat(tools): LoadTrees

--- tools/tools.py
+++ tools/tools.py
@@ -38,8 +38,24 @@ def CreateTrees(
         except:
             print("Failed to create a tree..!")
 
-def LoadTrees():
+def LoadTrees(
+    tree_directory : str,
+    extension = "csv", 
+    sampling_rate = 1.0, 
+    
+):
+    treefiles = sorted(Path(tree_directory).glob("*."+extension))
+    ext = ('.'+extension)
+    data = pd.DataFrame()    
 
+    for path, dirc, files in os.walk(tree_directory):
+        sampled_files = random.choices(files, k=round(len(id_set)*0.1))
+        for name in sampled_files:
+            if name.endswith(ext):
+                file_names.append(name) 
+                tmp = pd.read_csv(data_dir+name, sep=',', header=0, low_memory=False) 
+                data = pd.concat([data, tmp], ignore_index=True)
+                
 def Train():
 
 
@@ -48,25 +64,3 @@ def Train():
 
 
 
-
-###### load all data sets
-
-file_names = []
-data_dir= '/home/hgan/notebooks/delaysp_hpf/trees/'
-path_of_the_directory = data_dir
-ext = ('tree.csv')
-data = pd.DataFrame()
-
-for path, dirc, files in os.walk(path_of_the_directory):
-    sampled_files = random.choices(files, k=round(len(id_set)*0.1))
-    for name in sampled_files:
-        if name.endswith(ext):
-            file_names.append(name)
-            #print(name[:-4])
-            print(name)
-#             try:                
-            tmp = pd.read_csv(data_dir+name, sep=',', header=0, low_memory=False) 
-            #tmp = tmp.assign(LST=lambda x: tmp.iloc[:,-1] + int(find_between( name, '_t00', '-I-')))
-            data = pd.concat([data, tmp], ignore_index=True)
-            #data = data.append(tmp, ignore_index=True)
-            print('appending..')
